2cebdf2c50794a5fce01d86cbd3ecc6c4e9abcef
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-05-06 21:37:06 +0200

    Update description for input field

--- bin/gui.rs
+++ bin/gui.rs
@@ -3,6 +3,6 @@ use klask::Settings;
 
 fn main() {
     let mut settings = Settings::default();
-    settings.enable_stdin = Some("Description".to_string());
+    settings.enable_stdin = Some("Text to be sent, line by line".to_string());
     klask::run_derived::<App, _>(settings, process);
 }
