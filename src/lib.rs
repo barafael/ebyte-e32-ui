015447791f2c6e8a1298b4265a6c3abb1fe0f99c
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-10-29 17:24:52 +0200

    Update to readline v10

--- src/lib.rs
+++ src/lib.rs
@@ -43,35 +43,47 @@ pub fn process(args: App) {
             io::stdout().flush().unwrap();
         }
     } else {
-        let mut prompt = Editor::<()>::new();
-        loop {
-            match prompt.readline("Enter message >> ") {
-                Ok(line) => {
-                    if line == "exit" || line == "quit" {
-                        break;
-                    }
-                    prompt.add_history_entry(&line);
+        send(ebyte)
+    }
+}
 
-                    for b in line.as_bytes() {
-                        block!(ebyte.write(*b)).unwrap();
-                        print!("{}", *b as char);
-                        io::stdout().flush().unwrap();
-                    }
-                    block!(ebyte.write(b'\n')).unwrap();
-                    println!();
-                }
-                Err(ReadlineError::Interrupted) => {
-                    println!("CTRL-C");
+fn send<S, Aux, M0, M1, D>(mut ebyte: Ebyte<S, Aux, M0, M1, D, ebyte_e32::mode::Normal>)
+where
+    S: embedded_hal::serial::Read<u8> + embedded_hal::serial::Write<u8>,
+    <S as embedded_hal::serial::Write<u8>>::Error: std::fmt::Debug,
+    Aux: embedded_hal::digital::v2::InputPin,
+    M0: embedded_hal::digital::v2::OutputPin,
+    M1: embedded_hal::digital::v2::OutputPin,
+    D: embedded_hal::blocking::delay::DelayMs<u32>,
+{
+    let mut prompt = Editor::<()>::new().expect("Failed to set up prompt");
+    loop {
+        match prompt.readline("Enter message >> ") {
+            Ok(line) => {
+                if line == "exit" || line == "quit" {
                     break;
                 }
-                Err(ReadlineError::Eof) => {
-                    println!("CTRL-D");
-                    break;
+                prompt.add_history_entry(&line);
+
+                for b in line.as_bytes() {
+                    block!(ebyte.write(*b)).unwrap();
+                    print!("{}", *b as char);
+                    io::stdout().flush().unwrap();
                 }
-                Err(err) => {
-                    println!("Error: {:?}", err);
-                    break;
-                }
+                block!(ebyte.write(b'\n')).unwrap();
+                println!();
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
             }
         }
     }
